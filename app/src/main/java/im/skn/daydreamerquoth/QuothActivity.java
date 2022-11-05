3955080d91a1706ecc021a132d0bd18fb91a2383
Srijith Nair <660462+skn@users.noreply.github.com>
2022-11-05 22:18:48 +0400

    show screen saver setting instead of display setting on app launch

--- app/src/main/java/im/skn/daydreamerquoth/QuothActivity.java
+++ app/src/main/java/im/skn/daydreamerquoth/QuothActivity.java
@@ -1,6 +1,7 @@
 package im.skn.daydreamerquoth;
 
 import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -11,7 +12,7 @@ public class QuothActivity extends Activity {
 
     public void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-        startActivity(new Intent("android.settings.DISPLAY_SETTINGS"));
+        startActivity(new Intent("android.settings.DREAM_SETTINGS"));
         finish();
     }
 }
